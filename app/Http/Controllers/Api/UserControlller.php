f62fbf6b6b9f8178ad57bf28ac053b8cee0a8e6b
adi surya <adisurya1@gmail.com>
2025-02-01 11:05:26 +0700

    add users endpoint

---
+++ app/Http/Controllers/Api/UserControlller.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\User;
+
+class UserControlller extends Controller
+{
+    public function index(Request $request) {
+        $users = User::paginate();
+
+        return response()->json($users);
+
+    }
+
+
+}
--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\Api\TaskController;
+use App\Http\Controllers\Api\UserControlller;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -18,5 +19,7 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::get('tasks/my-tasks-notification', [TaskController::class,'myTaskNotification'])->name('tasks.my-tasks-notification');
 
     Route::apiResource('tasks', TaskController::class);
+    Route::get('users', [UserControlller::class,'index'])->name('users.index');
+
     Route::post('/logout', [AuthController::class, 'logout']);
 });
